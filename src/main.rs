dc0900eef7f42d7361b9a1626cc38be2ffaa1320
akthrms <akt.hrms@gmail.com>
2023-05-10 01:41:24 +0900

    refactor: 戻り値の型をユニットに変更

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::net::UdpSocket;
 use toy_dns_server::handle_query;
 
-fn main() -> anyhow::Result<()> {
-    let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
+fn main() {
+    let socket = UdpSocket::bind(("0.0.0.0", 2053)).expect("couldn't bind to address");
 
     loop {
         if let Err(e) = handle_query(&socket) {
